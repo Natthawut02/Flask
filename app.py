a96806459cef2273184d68e3e335d7ac5a5c5eba
Natthawut Thanomkaew <natthawutthanomkaew@gmail.com>
2025-02-15 21:10:40 +0700

    Add a subscribe button

--- app.py
+++ app.py
@@ -6,25 +6,22 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
-# สร้าง Model สำหรับ Users
+
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(100), unique=True, nullable=False)
     password = db.Column(db.String(100), nullable=False)
 
-# Route หน้าแรก
 @app.route('/')
 def home():
     return render_template('index.html')
 
-# Route Login
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
         
-        # ค้นหาผู้ใช้ในฐานข้อมูล
         user = User.query.filter_by(username=username, password=password).first()
         if user:
             return redirect(url_for('home'))
@@ -33,7 +30,23 @@ def login():
 
     return render_template('login.html')
 
-# สร้างฐานข้อมูล (ถ้ายังไม่มี)
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        
+        if User.query.filter_by(username=username).first():
+            return render_template('register.html', error="ชื่อผู้ใช้นี้มีอยู่แล้ว!")
+        
+        new_user = User(username=username, password=password)
+        db.session.add(new_user)
+        db.session.commit()
+        
+        return redirect(url_for('login'))
+    
+    return render_template('register.html')
+
 with app.app_context():
     db.create_all()
 
